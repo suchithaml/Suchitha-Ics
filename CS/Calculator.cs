e37cd9ad8f13386e9eb440904f15137af8f5c4e2
suchithaml <130212227+suchithaml@users.noreply.github.com>
2026-06-09 12:20:51 +0530

    Add files via upload

---
+++ CS/Calculator.cs
@@ -0,0 +1,9 @@
+namespace DemoProject.Utilities
+{
+    public class Calculator
+    {
+        public int Add(int a, int b) => a + b;
+        public int Subtract(int a, int b) => a - b;
+        public int Multiply(int a, int b) => a * b;
+    }
+}
---
+++ CS/Employee.cs
@@ -0,0 +1,9 @@
+namespace DemoProject.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public string Department { get; set; }
+    }
+}
