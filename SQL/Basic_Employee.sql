3a68e210669ed0e4b2241f9b01c0b6182e42d811
suchithaml <130212227+suchithaml@users.noreply.github.com>
2026-06-15 10:33:36 +0530

    Add files via upload
    Added JoinDate column - Version 3

--- SQL/Basic_Employee.sql
+++ SQL/Basic_Employee.sql
@@ -1,12 +1,15 @@
--- Version 2: Added Department column
+-- Version 3: Added JoinDate and extra employees
 CREATE TABLE Employees
 (
     EmpID    INT PRIMARY KEY,
     Name     VARCHAR(50),
     Salary   DECIMAL(10,2),
-    DeptID   INT
+    DeptID   INT,
+    JoinDate DATE
 );
 
-INSERT INTO Employees VALUES (101, 'Arun',    50000, 1);
-INSERT INTO Employees VALUES (102, 'Bharath', 40000, 2);
-INSERT INTO Employees VALUES (103, 'Charan',  70000, 1);
+INSERT INTO Employees VALUES (101, 'Arun',    50000, 1, '2023-01-10');
+INSERT INTO Employees VALUES (102, 'Bharath', 40000, 2, '2022-06-15');
+INSERT INTO Employees VALUES (103, 'Charan',  70000, 1, '2024-03-01');
+INSERT INTO Employees VALUES (104, 'Deepak',  65000, 3, '2021-09-20');
+INSERT INTO Employees VALUES (105, 'Esha',    55000, 2, '2023-11-05');
